f76cf5ab2b63de457b1461671cd168d5fcffb2b6
ahaic <g>
2012-12-24 00:50:34 +0800

    修改 格式

--- application/controllers/floor_plan.php
+++ application/controllers/floor_plan.php
@@ -7,7 +7,7 @@ class Floor_plan extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('drupal/page');
-		//$this->load->view('drupal/floor_plan');
+		$this->load->view('drupal/floor_plan');
 		
 	
 		$this->load->view('drupal/footer');
--- application/views/drupal/floor_plan.php
+++ application/views/drupal/floor_plan.php
@@ -4,8 +4,7 @@
     <nav class="breadcrumb"><a href="/drupal/">Home</a> » Floor Plan</nav>
     </div>   
     
-           <h1 class="page-title">About </h1>                                            
-      
+
     
   <div class="content node-page">
     <div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><p>asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sfasdf sf asf sdf  sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf df sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sfasdf sf asf sdf  sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf df sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sfasdf sf asf sdf  sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf df sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sfasdf sf asf sdf  sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf df sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sfasdf sf asf sdf  sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf df sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sfasdf sf asf sdf  sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf df sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sfasdf sf asf sdf  sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf asdf sf asf sdf</p>
@@ -47,77 +46,6 @@
  <!-- /.region -->
     </aside>  <!-- /#sidebar-first -->
     
-  <div class="clear"></div>
+ 
   
-      <div id="footer-saran" class="clearfix">
-     <div id="footer-wrap">
-            <div class="footer-box"><div class="region region-footer-first">
-  <div id="block-block-2" class="block block-block">
-
-        <h2 >We Do</h2>
-    
-  <div class="content">
-    <ul><li> Real Estate </li>
-<li> Construction </li>
-<li> Disposing </li>
-<li> Construction </li>
-</ul>  </div>
-  
-</div> <!-- /.block -->
-</div>
- <!-- /.region -->
-</div>
-                  <div class="footer-box"><div class="region region-footer-second">
-  <div id="block-block-1" class="block block-block">
-
-        <h2 >We are on</h2>
-    
-  <div class="content">
-    <ul><li>Twitter</li>
-<li>Facebook</li>
-<li>Google +</li>
-</ul>  </div>
-  
-</div> <!-- /.block -->
-</div>
- <!-- /.region -->
-</div>
-                  <div class="footer-box"><div class="region region-footer-third">
-  <div id="block-block-4" class="block block-block">
-
-        <h2 >We Provide</h2>
-    
-  <div class="content">
-    <ul><li>Timely </li>
-<li>Quality </li>
-<li>Responsibility </li>
-</ul>  </div>
-  
-</div> <!-- /.block -->
-</div>
- <!-- /.region -->
-</div>
-                 </div>
-    </div>
-    <div class="clear"></div>
-    
-  <!--END footer -->
-  <div class="region region-footer">
-  <div id="block-system-powered-by" class="block block-system">
-
-      
-  <div class="content">
-    <span>Powered by <a href="http://drupal.org">Drupal</a></span>  </div>
-  
-</div> <!-- /.block -->
-</div>
- <!-- /.region -->
-
-    <div class="clear"></div>
-  <div id="copyright">
-          Copyright &copy; 2012, G&amp;M Construction.
-              <span class="credits">Designed by  <a href="http://www.devsaran.com">Devsaran</a>.</span>
-      </div>
-  </div>
-  </body>
-</html>
+     
