3cecdddd30cd193457a3f47e43e9ff7bd1346c08
ahaic <g>
2012-12-25 22:39:48 +0800

    x

--- application/views/drupal/page.php
+++ application/views/drupal/page.php
@@ -1,25 +1,22 @@
-<!--   This page is header for ohter pages except home -->
 <!DOCTYPE html>
+
 <head>
 <meta charset="utf-8" />
+
 <title>Welcome to G&amp;M Construction | G&amp;M Construction</title>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>/assets/business/style.css" charset="utf-8"/>  
-
-<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-
-<script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/sliding_effect.js"></script>
-
-<script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/superfish.js?mdohm1"></script>
-<script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/supersubs.js?mdohm1"></script>
-<script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/sfmenu.js?mdohm1"></script>
-
-
 
 <script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/jquery.js?v=1.4.4"></script>
 <script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/query.once.js?v=1.2"></script>
 <script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/drupal.js?mdohm1"></script>
 
+<script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/sliding_effect.js"></script>
+<script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/superfish.js?mdohm1"></script>
+<script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/supersubs.js?mdohm1"></script>
+<script type="text/javascript" src="<?php echo base_url()?>/assets/business/js/sfmenu.js?mdohm1"></script>
 
+
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 
 
 </head>
@@ -36,10 +33,10 @@
 
     <div>
              <div id="logo">
-             <a href="/drupal/" title="Home"><img src="<?php echo base_url()?>assets/business/logo.png" alt="Home" /></a>
+             <a href="<?php echo site_url('')?>" title="Home"><img src="<?php echo base_url()?>assets/business/logo.png" alt="Home" /></a>
         </div>
             <hgroup id="sitename">
-        <h2><a href="<?php echo site_url('drupal')?>" title="Home">G&amp;M Construction</a></h2>
+        <h2><a href="<?php echo site_url('')?>" title="Home">G&amp;M Construction</a></h2>
         <p>a construction company</p><!--site slogan-->
       </hgroup>
     </div>
@@ -48,7 +45,7 @@
  <nav id="navigation" class="clearfix" role="navigation">
       <div id="main-menu">
         <ul class="menu">
-<li class="first leaf"><a href="<?php echo site_url('drupal')?>" class="active">Home</a></li>
+<li class="first leaf"><a href="<?php echo site_url('')?>" class="active">Home</a></li>
 
 <li class="leaf"><a href="<?php echo  site_url('floor_plan')?>">Floor Plan</a></li>
 <li class="leaf"><a href="<?php echo site_url('display_location')?>">Display Location</a></li>
@@ -57,18 +54,10 @@
 </ul>     
  </div>
     </nav>
-    </nav><!-- end main-menu -->
+    
+ 
+    
+    <!-- end main-menu -->
   </header>
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+ 
