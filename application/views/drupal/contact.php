db6b7ba9d5211367f746b70f462e24ffc8caacac
ahaic <g>
2012-12-25 22:39:19 +0800

    test

--- application/views/drupal/contact.php
+++ application/views/drupal/contact.php
@@ -6,10 +6,8 @@
     <section id="post-content" role="main">
     
     <nav class="breadcrumb"><a href="<?php echo site_url('')?>">Home</a> » Contact</nav>
-    </div>               
-     
-     
-     
+ </div>               
+  
     <h1 class="page-title">Contact</h1>                   
    
  <br> G&M Construction </br>
