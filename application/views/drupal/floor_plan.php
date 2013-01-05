72a8d83d5ad4a010931c1092658fcfcd0a3f01ee
Jj <hansoncao@live.com>
2013-01-05 20:40:57 +0800

    floor paln
    s

--- application/views/drupal/floor_plan.php
+++ application/views/drupal/floor_plan.php
@@ -1,11 +1,11 @@
 
 		<link type="text/css" href="../../styles/css3.css" rel="stylesheet" />
 		<script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.js"></script>
-        <script type="text/javascript" src="../../lib/jquery.jcarousel.min.js"></script>
-		<script type="text/javascript" src="../../lib/jquery.pikachoose.js"></script>
+        <script type="text/javascript" src="<?php echo base_url()?>/assets/pikachoose/lib/jquery.jcarousel.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url()?>/assets/pikachoose/lib/jquery.pikachoose.js"></script>
 		
-        <script type="text/javascript" src="../../lib/jquery.fancybox-1.3.4.pack.js"></script> 
-   		<link rel="stylesheet" type="text/css" href="../../styles/jquery.fancybox-1.3.3.css.css" media="screen" />
+        <script type="text/javascript" src="<?php echo base_url()?>/assets/pikachoose/lib/jquery.fancybox-1.3.4.pack.js"></script> 
+   		<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>/assets/pikachoose/styles/jquery.fancybox-1.3.3.css.css" media="screen" />
 		
 		<script language="javascript">
 		
@@ -51,9 +51,9 @@ Styled without images. Pure CSS.
 
 
 	<ul id="pikame" >
-		<li><a href="../../../../../../Public/Pictures/Sample Pictures/Penguins.jpg"><img src="../../1.jpg"/></a><span>This is an example of the basic theme.</span></li>
-		<li><a href="http://www.pikachoose.com"><img src="../../2.jpg"/></a><span>jCarousel is supported and can be integrated with PikaChoose!</span></li>
-		<li><a href="http://www.pikachoose.com"><img src="../../3.jpg"/></a><span>Be sure to check out for updates.
+		<li><a href="#"><img src="<?php echo base_url()?>/assets/pikachoose/1.jpg"/></a><span>This is an example of the basic theme.</span></li>
+		<li><a href="http://www.pikachoose.com"><img src="<?php echo base_url()?>/assets/pikachoose/2.jpg"/></a><span>jCarousel is supported and can be integrated with PikaChoose!</span></li>
+		<li><a href="http://www.pikachoose.com"><img src="<?php echo base_url()?>/assets/pikachoose/3.jpg"/></a><span>Be sure to check out for updates.
         </span></li>
 	</ul>
 </div>
@@ -67,9 +67,9 @@ Styled without images. Pure CSS.
 
 
 	<ul id="pikama" >
-		<li><a href="../../../../../../Public/Pictures/Sample Pictures/Penguins.jpg"><img src="../../1.jpg"/></a><span>This is an example of the basic theme.</span></li>
-		<li><a href="http://www.pikachoose.com"><img src="../../2.jpg"/></a><span>jCarousel is supported and can be integrated with PikaChoose!</span></li>
-		<li><a href="http://www.pikachoose.com"><img src="../../3.jpg"/></a><span>Be sure to check out for updates.
+		<li><a href="#"><img src="<?php echo base_url()?>/assets/pikachoose/1.jpg"/></a><span>This is an example of the basic theme.</span></li>
+		<li><a href="http://www.pikachoose.com"><img src="<?php echo base_url()?>/assets/pikachoose/2.jpg"/></a><span>jCarousel is supported and can be integrated with PikaChoose!</span></li>
+		<li><a href="http://www.pikachoose.com"><img src="<?php echo base_url()?>/assets/pikachoose/3.jpg"/></a><span>Be sure to check out for updates.
         </span></li>
 	</ul>
 </div>
