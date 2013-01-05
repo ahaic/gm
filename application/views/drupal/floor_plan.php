6540708093cc7a05c7727376d6dd424b386bedcb
Jj <hansoncao@live.com>
2013-01-05 20:33:10 +0800

    floor plan
    f

--- application/views/drupal/floor_plan.php
+++ application/views/drupal/floor_plan.php
@@ -1,4 +1,28 @@
 
+		<link type="text/css" href="../../styles/css3.css" rel="stylesheet" />
+		<script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.js"></script>
+        <script type="text/javascript" src="../../lib/jquery.jcarousel.min.js"></script>
+		<script type="text/javascript" src="../../lib/jquery.pikachoose.js"></script>
+		
+        <script type="text/javascript" src="../../lib/jquery.fancybox-1.3.4.pack.js"></script> 
+   		<link rel="stylesheet" type="text/css" href="../../styles/jquery.fancybox-1.3.3.css.css" media="screen" />
+		
+		<script language="javascript">
+		
+			$(document).ready(function (){
+				var a =function(self){
+					self.anchor.fancybox();
+				};
+				
+					$("#pikame").PikaChoose({buildFinished:a});
+					$("#pikama").PikaChoose({buildFinished:a});
+					
+				});
+				
+				
+		</script>
+
+
 
 
 
@@ -9,14 +33,52 @@
     </div>               
      
      
-     
-    <h1 class="page-title">Floor Plan</h1>                   
+                    
    
  <br> G&M Construction </br>
  
 
-   
- 
+
+		
+</head>
+<body>
+<!-- not really needed, i'm using it to center the gallery. -->
+
+<div class="wrap">
+
+<div class="pikachoose">
+Styled without images. Pure CSS.
+
+
+	<ul id="pikame" >
+		<li><a href="../../../../../../Public/Pictures/Sample Pictures/Penguins.jpg"><img src="../../1.jpg"/></a><span>This is an example of the basic theme.</span></li>
+		<li><a href="http://www.pikachoose.com"><img src="../../2.jpg"/></a><span>jCarousel is supported and can be integrated with PikaChoose!</span></li>
+		<li><a href="http://www.pikachoose.com"><img src="../../3.jpg"/></a><span>Be sure to check out for updates.
+        </span></li>
+	</ul>
+</div>
+
+
+
+<hr /><p>
+
+<div class="pikachoose">
+Styled without images. Pure CSS.
+
+
+	<ul id="pikama" >
+		<li><a href="../../../../../../Public/Pictures/Sample Pictures/Penguins.jpg"><img src="../../1.jpg"/></a><span>This is an example of the basic theme.</span></li>
+		<li><a href="http://www.pikachoose.com"><img src="../../2.jpg"/></a><span>jCarousel is supported and can be integrated with PikaChoose!</span></li>
+		<li><a href="http://www.pikachoose.com"><img src="../../3.jpg"/></a><span>Be sure to check out for updates.
+        </span></li>
+	</ul>
+</div>
+
+</div> <!-- end of wrap -->
+
+</body>
+</html>
+
  
   
      
