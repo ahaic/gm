a1c71fe2a0011379e0e8312200c9dcff33e4e717
Jj <hansoncao@live.com>
2013-01-06 22:59:55 +0800

    s

--- application/views/drupal/contact.php
+++ application/views/drupal/contact.php
@@ -12,7 +12,7 @@
  
 
 <div class="staff">
-<table border="0" cellspacing="1" summary="" cellpadding="1" width="648" height="128" style="width: 648px; height: 128px">
+<table border="0" cellspacing="1" summary="" cellpadding="1" width="648" height="128" style="width: 600px; height: 128px">
     <tbody>
         <tr>
             <td width="120" align="center"><img border="0" alt="" width="79" height="120" src="http://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Tencent_QQ.png/64px-Tencent_QQ.png"></td>
@@ -21,7 +21,8 @@
             <br>
             Head of School</strong></span><span><br>
             Room 7008, Level 7,  Australia<br>
-            Phone: +61 7-334-61701; Fax: +61 7-334-61999<br>
+            Phone: +61 7-334-61701;<br> 
+            Fax: +61 7-334-61999<br>
             Email: Personal :&nbsp; <a href="mailto:c.kovacevic@pharmacy.uq.edu.au">example@money.com</a></span></p>
             </td>
         </tr>
@@ -35,7 +36,8 @@
             <br>
             Head of School</strong></span><span><br>
             Room 7008, Level 7,  Australia<br>
-            Phone: +61 7-334-61701; Fax: +61 7-334-61999<br>
+            Phone: +61 7-334-61701;<br>
+            Fax: +61 7-334-61999<br>
             Email: Personal :&nbsp; <a href="mailto:c.kovacevic@pharmacy.uq.edu.au">example@money.com</a></span></p>
             </td>
         </tr>
@@ -49,7 +51,8 @@
             <br>
             Head of School</strong></span><span><br>
             Room 7008, Level 7,  Australia<br>
-            Phone: +61 7-334-61701; Fax: +61 7-334-61999<br>
+            Phone: +61 7-334-61701; <br>
+            Fax: +61 7-334-61999<br>
             Email: Personal :&nbsp; <a href="mailto:c.kovacevic@pharmacy.uq.edu.au">example@money.com</a></span></p>
             </td>
         </tr>
